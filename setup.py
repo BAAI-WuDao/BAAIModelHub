9422ea70e9deacb3f7ddbeb191f4c7faa269f1cd
wnagguojim <862876363@qq.com>
2022-06-14 09:00:37 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="baai_modelhub",
+    version="0.0.1",
+    author="zhiyuan",
+    author_email="zhiyuan@baai.ac.cn",
+    description="BAAIModelHub is a tool for managing models from model.baai.ac.cn",
+    keywords="modelhub",
+    license="MIT",
+    url="https://github.com/BAAI-WuDao/BAAIModelHub",
+    packages=find_packages(),
+    python_requires=">=3.6.0",
+)
+
+
